1ec65f4aed09e50e83f7c11a644f8f7e624f96a3
Alan Kydd <kydd.alan.g@gmail.com>
2024-08-24 14:27:49 -0600

    Initial commit

---
+++ test/realworld_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns realworld-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [realworld-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

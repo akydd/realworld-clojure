ebac59a58e45c7b610813d927439fb2398242305
Alan Kydd <kydd.alan.g@gmail.com>
2024-11-19 22:35:17 -0700

    Basic user tests working
    DB is cleared between tests. Could not figure out how to work db
    transactions into the `with-system` macro, so I'm just truncating all
    the tables.

--- test/realworld_clojure/domain/user_test.clj
+++ test/realworld_clojure/domain/user_test.clj
@@ -2,9 +2,16 @@
   (:require
    [com.stuartsierra.component :as component]
    [realworld-clojure.config-test :as config]
+   [realworld-clojure.domain.user :as user]
+   [next.jdbc.sql :as sql]
    [clojure.test :refer :all]
+   [realworld-clojure.core :as core]
+   [clojure.core :as c]))
 
-   [realworld-clojure.core :as core]))
+(defn clear-ds
+  "Delete all data from datasource"
+  [ds]
+  (sql/query ds ["truncate comments, articles, users cascade"]))
 
 (defmacro with-system
   [[bound-var binding-expr] & body]
@@ -12,9 +19,18 @@
      (try
        ~@body
        (finally
+         (clear-ds (get-in ~bound-var [:database :datasource]))
          (component/stop ~bound-var)))))
 
-(deftest greeting-test
-  (with-system 
+(deftest register-invalid-input
+  (with-system
     [sut (core/new-system (config/read-test-config))]
-    (is (= 0 0))))
+    (is (c/contains? (user/register-user (:user-controller sut) {}) :errors))))
+
+(deftest register-already-exists
+  (with-system
+    [sut (core/new-system (config/read-test-config))]
+    (let [db (get-in sut [:database :datasource])
+          test-user {:username "user2" :email "user2@user" :password "password"}
+          _ (sql/insert! db :users test-user)]
+      (is (thrown-with-msg? Exception #"duplicate" (user/register-user (:user-controller sut) test-user))))))
